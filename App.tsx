ce45f396d0c4b3d4d61f6bbf8787a1028d149a71
Igor Cotrim <igorxuxicotrim@gmail.com>
2022-05-06 23:09:13 -0300

    create gestures button

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import 'react-native-gesture-handler'
+
 import { View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import AppLoading from 'expo-app-loading';
@@ -7,7 +9,7 @@ import {
   Inter_500Medium
 } from '@expo-google-fonts/inter';
 
-import { Widget } from './src/components/Widget';
+import Widget from './src/components/Widget';
 import { theme } from './src/theme';
 
 export default function App() {
@@ -25,13 +27,13 @@ export default function App() {
       flex: 1,
       backgroundColor: theme.colors.background
     }}>
-      <Widget />
-
       <StatusBar
         style="light"
         backgroundColor='transparent'
         translucent
       />
+
+      <Widget />
     </View>
   );
 }
